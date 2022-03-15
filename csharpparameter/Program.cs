f0c5c477376af8b3758964adb4de9a6dfa75ff30
Damon <Damon@LAPTOP-G7H6B9OO>
2022-03-15 13:32:56 +0100

    Add project files.

---
+++ csharpparameter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void MyMethod(string Callist)
+        {
+            Console.WriteLine(Callist + "cfghj");
+        }
+        static void Main(string[] args) 
+        {
+            MyMethod("");
+            MyMethod("");
+            MyMethod("");
+            MyMethod("");
+            Console.ReadLine();
+        }
+    }
+}
